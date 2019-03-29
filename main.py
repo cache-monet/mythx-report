49949248444604086eb3562a8b5d2cc94ef2223f
cache-monet <remiaston@gmail.com>
2019-03-29 10:35:06 -0400

    add time

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def generate_report(date_from: datetime = None, date_to: datetime = None):
     else: report_range = "All time"
     analyses = c.analysis_list(date_from=date_from, date_to=date_to)
 
+    # Report title
     click.echo("=========================================")
     click.echo(f"Analyses report: {report_range}")
     click.echo("=========================================")
@@ -46,13 +47,14 @@ def generate_report(date_from: datetime = None, date_to: datetime = None):
             continue 
         resp = c.report(analysis.uuid)
         file_to_issue = defaultdict(list)
+        formatted_date = analysis.submitted_at.strftime("%m/%d/%Y, %H:%M")
         for issue in resp.issues:
             source_locs = [loc.source_map.split(":") for loc in issue.locations]
             source_locs = [(int(o), int(l), int(i)) for o, l, i in source_locs]
             for _, _, file_idx in source_locs:
                 filename = resp.source_list[file_idx]
                 file_to_issue[filename].append(
-                    (issue.swc_title, issue.severity, issue.description_short)
+                    (issue.swc_id, issue.swc_title, issue.severity, issue.description_short, formatted_date)
                 )
         detectedIssues += len(resp.issues)
         for filename, data in file_to_issue.items():
@@ -62,14 +64,19 @@ def generate_report(date_from: datetime = None, date_to: datetime = None):
                     data,
                     tablefmt="rst",
                     headers=(
+                        "SWC ID",
                         "SWC Title",
                         "Severity",
                         "Short Description",
+                        "Time Submitted",
                     ),
                 )
             )
             click.echo("\n")
-    click.echo(f"Total analyses: {len(analyses)}; Detected issues: {detectedIssues}; Failed analyses: {failed_analyses}")
+    click.echo("--------")
+    click.echo("Summary:")
+    click.echo("--------")
+    click.echo(f"Total analyses: {analyses.total}; Detected issues: {detectedIssues}; Failed analyses: {failed_analyses}")
 
 @click.group()
 def main():
